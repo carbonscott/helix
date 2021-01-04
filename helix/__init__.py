1cee272df4a6c35e47f22efcb3b2eb98a380b713
Cong Wang <wangimagine@gmail.com>
2021-01-03 23:35:56 -0600

    Include peptide (more than a single atom case);

--- helix/__init__.py
+++ helix/__init__.py
@@ -47,7 +47,7 @@ def helix(parvals, num, pt0):
         pt0 is the beginning position of the helix.
     '''
     # Unpack parameters...
-    px, py, pz, nx, ny, nz, r, s, omega, phi, t = parvals
+    px, py, pz, nx, ny, nz, s, omega, r, phi, t = parvals
 
     # Consider phase shift...
     psi_list = np.array([ omega * i for i in range(num) ])
@@ -87,25 +87,67 @@ def residual(params, xyzs):
     # Avoid np.nan as the first valid point
     xyzs_nonan = xyzs[~np.isnan(xyzs).any(axis = 1)]
 
-    # Compute...
     # Unpack parameters...
     parvals = unpack_params(params)
+
+    # Compute...
     res   = helix(parvals, num, xyzs_nonan[0]) - xyzs
 
     return res.reshape(-1)
 
 
-def super_residual(params, xyzs):
-    # Calculate size of the helix...
-    num   = xyzs.shape[0]
+def residual_peptide(params, xyz_dict):
+    # Unpack parameters...
+    parvals = unpack_params(params)
+    px, py, pz, nx, ny, nz, s, omega = parvals[ :8]
+    rN, rCA, rC, rO                  = parvals[8:8+4]
+    phiN, phiCA, phiC, phiO          = parvals[8:8+4]
+    tN, tCA, tC, tO                  = parvals[8:8+4]
 
-    # Avoid np.nan as the first valid point
-    xyzs_nonan = xyzs[~np.isnan(xyzs).any(axis = 1)]
+    # Construct paramters for each atom...
+    parval_dict = {}
+    parval_dict["N"]  = px, py, pz, nx, ny, nz, s, omega, rN,  phiN,  tN
+    parval_dict["CA"] = px, py, pz, nx, ny, nz, s, omega, rCA, phiCA, tCA
+    parval_dict["C"]  = px, py, pz, nx, ny, nz, s, omega, rC,  phiC,  tC
+    parval_dict["O"]  = px, py, pz, nx, ny, nz, s, omega, rO,  phiO,  tO
 
-    # Compute...
-    res = helix(params, num, xyzs_nonan[0]) - xyzs
+    # Create dictionary to store values
+    num_dict        = {}
+    xyzs_nonan_dict = {}
+    res_dict        = {}
 
-    return res.reshape(-1)
+    # Computation for each type of atom...
+    for i in xyzs_dict.keys():
+        num_dict[i]  = xyz_dict[i].shape[0]
+
+        # Avoid np.nan as the first valid point
+        xyzs_nonan_dict[i] = xyz_dict[i][~np.isnan(xyz_dict[i]).any(axis = 1)]
+
+        # Compute...
+        res = helix(parval_dict[i], num_dict[i], xyzs_nonan_dict[i][0]) - xyz_dict[i]
+        res_dict[i] = res
+
+    # Format results for minimization...
+    num_coords = np.sum( [ v for _, v in num_dict.items() ] )
+    res_matrix = np.zeros( (num_coords, 3) )
+
+    # Assign values...
+    idx = 0
+    for i, v in res_dict.items():
+        res_matrix[idx:idx + num_dict[i], :] = v
+        idx += num_dict[i]
+
+    return res_matrix.reshape(-1)
+
+
+def fit_peptide(params, xyz_dict, **kwargs):
+    result = lmfit.minimize(residual_peptide, 
+                            params, 
+                            method     = 'least_squares', 
+                            nan_policy = 'omit',
+                            args       = (xyz_dict, ), 
+                            **kwargs)
+    return result
 
 
 def fit(params, xyzs, **kwargs):
@@ -118,13 +160,120 @@ def fit(params, xyzs, **kwargs):
     return result
 
 
+def peptide(xyz_dict):
+    ''' Fit a helix to amino acid according to input atomic positions.
+    '''
+    # Predefined helix parameters for amino acid...
+    s     = 5.5
+    omega = 1.7
+    rN, rCA, rC, rO = 1.6, 1.6, 1.6, 1.6
+    phiN, phiCA, phiC, phiO = np.pi + omega, np.pi + omega, np.pi + omega, np.pi + omega
+
+    # Avoid nan when estimating t
+    t = {}
+    for i in xyzs_dict.keys():
+        xyzs_nonan_dict[i] = xyz_dict[i][~np.isnan(xyz_dict[i]).any(axis = 1)]
+        t[i] = - 0.5 * np.linalg.norm(xyzs_nonan_dict[i][-1] - xyzs_nonan_dict[i][0])
+    tN  = t["N"]
+    tCA = t["CA"]
+    tC  = t["C"]
+    tO  = t["O"]
+
+    # Define init_values...
+    # Assume helix axis passes through the average position of a straight helix
+    ####
+    ###  TODO: how to assign different t value so that each chain has a
+    ###  different center
+    ###
+    ###  TODO: complete this scenario
+    ####
+    px, py, pz = np.nanmean(xyzs, axis = 0)
+
+    # Estimate the helix axis vector...
+    nv = estimate_axis(xyzs)
+    nx, ny, nz = nv
+
+    # Init params...
+    params = init_params()
+
+    # Load init values
+    params.add("px",    value = px)
+    params.add("py",    value = py)
+    params.add("pz",    value = pz)
+    params.add("nx",    value = nx)
+    params.add("ny",    value = ny)
+    params.add("nz",    value = nz)
+    params.add("s" ,    value = s)
+    params.add("omega", value = omega)
+    params.add("r" ,    value = r)
+    params.add("phi",   value = phi)
+    params.add("t",     value = t)
+
+    # Set constraints...
+    for i in params.keys(): params[i].set(vary = False)
+
+    # Fitting process...
+    report_params(params, title = f"Init report")
+
+    for i in ["px", "py", "pz"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"px, py, pz: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in ["phi"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"phi: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in ["s", "omega"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"s, omega: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in ["t"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"t: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in ["r"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"r: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in ["nx", "ny", "nz"]: params[i].set(vary = True)
+    result = fit(params, xyzs)
+    report_params(params, title = f"nx, ny, nz: " + \
+                                  f"success = {result.success}, " + \
+                                  f"cost = {result.cost}")
+    params = result.params
+
+    for i in range(5):
+        result = fit(params, xyzs, ftol = 1e-9)
+        report_params(params, title = f"All params: " + \
+                                      f"success = {result.success}, " + \
+                                      f"cost = {result.cost}")
+        params = result.params
+
+    return result
+
+
 def protein(xyzs):
     ''' Fit a helix to amino acid according to input atomic positions.
     '''
     # Predefined helix parameters for amino acid...
-    r             = 1.6
     s             = 5.5
     omega         = 1.7
+    r             = 1.6
     phi           = np.pi + omega
 
     # Avoid nan when estimating t
@@ -149,9 +298,9 @@ def protein(xyzs):
     params.add("nx",    value = nx)
     params.add("ny",    value = ny)
     params.add("nz",    value = nz)
-    params.add("r" ,    value = r)
     params.add("s" ,    value = s)
     params.add("omega", value = omega)
+    params.add("r" ,    value = r)
     params.add("phi",   value = phi)
     params.add("t",     value = t)
 
@@ -235,7 +384,7 @@ def check_fit(params, xyzs, pv0, nv0, nterm):
     q = helix(parvals, xyzs.shape[0], xyzs_nonan[0])
 
     # Unpack parameters...
-    px, py, pz, nx, ny, nz, r, s, omega, phi, t = parvals
+    px, py, pz, nx, ny, nz, s, omega, r, phi, t = parvals
     pv = np.array([px, py, pz])
     nv = np.array([nx, ny, nz])
 
@@ -295,7 +444,7 @@ def check_select(params, xyzs, pv0, nv0, nterm, bindex, helixlen):
     q = helix(parvals, xyzs_sel.shape[0], xyzs_sel_nonan[0])
 
     # Unpack parameters...
-    px, py, pz, nx, ny, nz, r, s, omega, phi, t = parvals
+    px, py, pz, nx, ny, nz, s, omega, r, phi, t = parvals
     pv = np.array([px, py, pz])
     nv = np.array([nx, ny, nz])
 
@@ -353,9 +502,9 @@ def report_params(params, title = ""):
     nx    = params["nx"].value
     ny    = params["ny"].value
     nz    = params["nz"].value
-    r     = params["r" ].value
     s     = params["s"].value
     omega = params["omega"].value
+    r     = params["r" ].value
     phi   = params["phi"].value
     t     = params["t"].value
 
@@ -364,9 +513,9 @@ def report_params(params, title = ""):
     print(f"----------  --------")
     print(f"px, py, pz: {px:<10.3f}    {py:<10.3f}    {pz:<10.3f}")
     print(f"nx, ny, xz: {nx:<10.3f}    {ny:<10.3f}    {nz:<10.3f}")
-    print(f"r:          {r:<10.3f}")
     print(f"s:          {s:<10.3f}")
     print(f"omega:      {omega:<10.3f}")
+    print(f"r:          {r:<10.3f}")
     print(f"phi:        {phi:<10.3f}")
     print(f"t:          {t:<10.3f}")
     print("")
