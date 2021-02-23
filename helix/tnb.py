6761e15cf7a648488a952c534fff2a037f30d9a6
Cong Wang <wangimagine@gmail.com>
2021-02-23 17:53:10 -0600

    Resize axial vector to unit length

--- helix/tnb.py
+++ helix/tnb.py
@@ -42,7 +42,9 @@ def measure_spline(xyzs):
     db_div_dt = norm_db / norm_dt
     rt = (1 + db_div_dt)**(-2)
     rb = db_div_dt * rt
-    avec = rt * bvec[:, :-1] + rb * tvec[:, :-1]
+    avec_aux  = rt * bvec[:, :-1] + rb * tvec[:, :-1]
+    norm_avec = np.linalg.norm(avec_aux, axis = 0, keepdims = True)
+    avec = avec_aux / norm_avec
 
     # Obtain curvature...
     curv = norm_nvec_aux / norm_dr1
