e77facbec076251a492c7dbd5dd5c635302a1f5b
Cong Wang <wangimagine@gmail.com>
2021-03-25 16:57:36 -0500

    Add support for calculating interhelical angle and distance, respectively

--- helix/core.py
+++ helix/core.py
@@ -36,4 +36,58 @@ def measure_twocores(core_xyz_ref, core_xyz_tar):
     return ih_dvec, norm_ih_dvec, core_vec_ref_unit, core_vec_tar_unit, ih_ang
 
 
+def calc_interangle(core_xyz_ref, core_xyz_tar):
+    ''' Measure the following aspects of two helical cores.
+        - Interhelical angle (0-90 degree)
+    '''
+    # Obtain the helical core vectors...
+    core_xyz_ref_nonan = remove_nan(core_xyz_ref)
+    core_xyz_tar_nonan = remove_nan(core_xyz_tar)
+    core_vec_ref = core_xyz_ref_nonan[-1] - core_xyz_ref_nonan[0]
+    core_vec_tar = core_xyz_tar_nonan[-1] - core_xyz_tar_nonan[0]
 
+    # Calculate the interhelical angle...
+    core_vec_ref_unit = core_vec_ref / np.linalg.norm(core_vec_ref)
+    core_vec_tar_unit = core_vec_tar / np.linalg.norm(core_vec_tar)
+    inter_angle = np.arccos( np.dot(core_vec_ref_unit, core_vec_tar_unit) )
+
+    return inter_angle
+
+
+def calc_interdist(core_xyz_ref, core_xyz_tar):
+    ''' Measure the following aspects of two helical cores.
+        - Interhelical distance vector between the centers.
+
+        Refers to http://geomalgorithms.com/a07-_distance.html for the method.
+        Q is ref, P is tar.  
+    '''
+    # Obtain the helical core vectors...
+    core_xyz_ref_nonan = remove_nan(core_xyz_ref)
+    core_xyz_tar_nonan = remove_nan(core_xyz_tar)
+    core_vec_ref = core_xyz_ref_nonan[-1] - core_xyz_ref_nonan[0]
+    core_vec_tar = core_xyz_tar_nonan[-1] - core_xyz_tar_nonan[0]
+
+    # Obtain the starting point...
+    q0 = core_xyz_ref_nonan[0]
+    p0 = core_xyz_tar_nonan[0]
+    w0 = p0 - q0
+
+    # Obtain the directional vector with magnitude...
+    v = core_vec_ref
+    u = core_vec_tar
+
+    # Math part...
+    a = np.dot(u, u)
+    b = np.dot(u, v)
+    c = np.dot(v, v)
+    d = np.dot(u, w0)
+    e = np.dot(v, w0)
+    de = a * c - b * b    # Denominator
+    if de == 0: sc, tc = 0, d / b
+    else:       sc, tc = (b * e - c * d) / de, (a * e - b * d) / de
+
+    # Calculate distance...
+    wc = w0 + sc * u - tc * v
+    inter_dist = np.linalg.norm(wc)
+
+    return inter_dist
